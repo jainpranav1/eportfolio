7a425f6580c4e9a0aa16c8e019873743f4e7599c
jainpranav1 <pranavji@tamu.edu>
2022-07-10 22:07:55 -0500

    added arrow key functionality

---
+++ gData.js
@@ -0,0 +1,28 @@
+let gData = {
+  nodes: [
+    {
+      id: "id1",
+      html: `
+      <h1>heading 1</h1>
+      <p>paragraph 1</p>
+      `,
+    },
+    {
+      id: "id2",
+      html: `
+      <h1>heading 2</h1>
+      <p>paragraph 2</p>
+      `,
+    },
+    {
+      id: "id3",
+      html: `
+      <h1>heading 3</h1>
+      <p>paragraph 3</p>
+      `,
+    },
+  ],
+  links: [],
+};
+
+exports.gData = gData;
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
-const fs = require("fs");
+const myModule = require("./gData.js");
 
 const app = express();
 app.set("view engine", "ejs");
 
-let rawdata = fs.readFileSync("gData.json");
-let gData = JSON.parse(rawdata);
-
 app.get("/", (req, res) => {
-  res.render("index", { gData: gData });
+  res.render("index", { gData: myModule.gData });
 });
 
 app.listen(5000);
