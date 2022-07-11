7d792b82b87e22b446e3f8aeec6046eab097b7da
jainpranav1 <pranavji@tamu.edu>
2022-07-10 23:39:30 -0500

    added more text

--- gData.js
+++ gData.js
@@ -1,21 +1,19 @@
 let gData = {
   nodes: [
     {
-      id: "id1",
       html: `
-      <h1>heading 1</h1>
-      <p>paragraph 1</p>
+      <h1>Pranav Jain</h1>
+      <p>Hello. Welcome to my site!</p>
       `,
     },
     {
-      id: "id2",
       html: `
-      <h1>heading 2</h1>
-      <p>paragraph 2</p>
+      <h1>About Me</h1>
+      <p>Hello! Thank you for taking the time to visit my site. My name is Pranav Jain and I am a CSCE student at Texas A&M University.<p>
+      <p>Since I was young, I have always loved to build stuff. In middle school, I would spend my weekends following build tutorials by Steve Spangler and Kip Kay on Youtube. In high school, I took the game design and robotics courses offered by my high school. This passion for building stuff led me to choose computer science as my major.<p>
       `,
     },
     {
-      id: "id3",
       html: `
       <h1>heading 3</h1>
       <p>paragraph 3</p>
